5b67f71301343831c7e180fbc48f8f23798c0fd8
shunki <tsuchiya.shunki@gmail.com>
2022-05-21 16:39:29 +0900

    NewsAPIで取得した記事の表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,35 @@
+import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
-import Layout from '../layouts';
+import MainLayout from '../layouts';
+import Article from '../components/article';
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <Layout>test</Layout>
+    <MainLayout>
+      <Head>
+        <title>Simple News</title>
+      </Head>
+      <div className={styles.main}>
+        <Article title="headLines" articles={props.topArticles} />
+      </div>
+    </MainLayout>
   )
 }
+
+export const getStaticProps = async () => {
+  // NewsAPIのトップ記事の情報を取得
+  const pageSize = 10;
+  const apiKey = '42a8b39dc06a4ecf8e10a5908de4e2b5';
+  const topReq = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${apiKey}`
+  )
+  const topJson = await topReq.json();
+  const topArticles = topJson?.articles;
+
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  }
+};
